4b4c178a164526e2c4c44baa11a1456cf5283831
stefano <stefano@172.20.94.40>
2017-05-08 09:41:15 +0200

    Nuovo metodo di creazione della connesione

--- Lab08_CountryBorders/src/it/polito/tdp/borders/db/DBConnect.java
+++ Lab08_CountryBorders/src/it/polito/tdp/borders/db/DBConnect.java
@@ -5,11 +5,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.DataSources;
+
 public class DBConnect {
 
 	static private final String jdbcUrl = "jdbc:mysql://localhost/countries?user=root&password=root";
 	static private DBConnect instance = null;
-
+	private static DataSource ds;
+	
 	private DBConnect() {
 		instance = this;
 	}
@@ -23,9 +28,18 @@ public class DBConnect {
 	}
 
 	public Connection getConnection() {
-		try {
-
-			Connection conn = DriverManager.getConnection(jdbcUrl);
+		
+		if(ds==null) //creare il dataSource, la prima volta che viene creato
+			try{
+			ds = DataSources.pooledDataSource(DataSources.unpooledDataSource(jdbcUrl));
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+		try { //utilizzo il dataSource gia creato per non chiamarlo tutte le volte
+			
+			Connection conn = ds.getConnection();
 			return conn;
 
 		} catch (SQLException e) {
